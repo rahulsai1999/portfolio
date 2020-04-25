03402e99aa79541a01ab2277a4f0799776ddd8f3
rahulsai1999 <noneuser2183@gmail.com>
2020-04-25 11:20:11 +0530

    example for graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -14,8 +14,22 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <p>{JSON.stringify(data)}</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    rickandmorty {
+      locations(page: 1) {
+        results {
+          id
+          name
+        }
+      }
+    }
+  }
+`
