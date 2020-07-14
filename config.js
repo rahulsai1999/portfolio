bd81262f6a95f075f958fd544cf6569fafe9f633
rahulsai1999 <noneuser2183@gmail.com>
2020-07-14 20:53:05 +0530

    portfolio update

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: 'Rahul Sai', // <title>
+  manifestName: 'Rahul Sai',
+  manifestShortName: 'Rahul Sai', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'R.S.Rahul ',
+  lastName: 'Sai',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/rahulsai1999',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/rahulsai99/',
+    },
+    {
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://www.instagram.com/rahul.sai/',
+    },
+  ],
+  email: 'rs.rahulsai23@gmail.com',
+  phone: '+91-9131038763',
+  address: 'Chennai, Tamil Nadu, IN',
+};
