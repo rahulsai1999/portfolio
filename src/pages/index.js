905d7caec2a91c54d63e9da1fa77f7c317f1a6dd
rahulsai1999 <noneuser2183@gmail.com>
2020-08-30 22:02:44 +0530

    relative import fix 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Layout from './components/layout';
-import Sidebar from './components/sidebar';
+import Sidebar from './components/Sidebar';
 import config from '../../config';
 
 const IndexPage = () => (
